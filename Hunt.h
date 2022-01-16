aea29eb5fb601d013af6eb208ee2449ee054a5fb
Marshall <syzdup@gmail.com>
2022-01-16 14:57:40 -0500

    Map input works

--- Hunt.h
+++ Hunt.h
@@ -1,15 +1,30 @@
-
 // IDENTIFIER: 40FB54C86566B9DDEAB902CC80E8CE85C1C62AAD
 // handles args and input for our program
 #include <iostream>
 #include <string>
+#include <vector>
 #include <getopt.h>
 
-// class to handle all options and args provided
+// class to handle all options and args provided. also reads in input files and stores data
 
 class Option_handler {
 
 private:
+    // implementation for reading in a map
+    // vector<vector<point>> 
+    // point: struct with land_type, been_visited
+    // file will be map or list of grid coords
+
+    // when the location added to the container is treasure, stop the search
+
+    struct Spot {
+        // type
+        // note: unsure if there is an easier way to represent the 'type' of spot
+        char spot_type;
+        // discovered
+        bool discovered = false;
+    };
+
     std::string captain_type = "STACK";
     std::string firstmate_type = "QUEUE";
     std::string hunt_order = "NESW";
@@ -18,6 +33,46 @@ private:
     bool stats_mode = false;
     bool show_path = false;
     std::string path_type;
+
+    // size of the map being read in (8 = 8x8)
+    int map_size;
+
+    // full map
+    std::vector<std::vector<Spot>> grid;
+
+    // private functions that do work for public "read_map()"
+    void read_map_file() {
+        std::cin >> map_size;
+        std::cout << "Map size: " << map_size << "\n";
+        std::string row;
+        // counter to keep track of row
+        int r_count = 0;
+        // counter to keep track of column
+        int c_count = 0;
+        // resize grid based on map size
+        grid.resize(map_size);
+        for(int i = 0; i < map_size; ++i) {
+            grid[i].resize(map_size);
+        }
+
+        // deal with 'junk' newline
+        std::string junk;
+        getline(std::cin, junk);
+
+        // read in each row and fill up grid
+        while(getline(std::cin, row)) {
+            c_count = 0;
+            for(char c: row) {
+                grid[r_count][c_count].spot_type = c;
+                ++c_count;
+            }
+            ++r_count;
+        }
+    } // read_map_file()
+
+    void read_list_file() {
+        std::cin >> map_size;
+    }
 
 public:
     // read in all of the options
@@ -110,7 +165,6 @@ public:
     // functions
     void print_help() {
         std::cout << "usage: ./hunt [options] < inputfile\n";
-
     }
 
     std::string get_captain_type() {
@@ -137,28 +191,34 @@ public:
         return show_path;
     }
 
+    void read_map() {
+        char map_type = ' ';
+        // read in comments until map_type is specified, then break
+        std::string comments;
+        while(getline(std::cin, comments)) {
+            if(comments[0] != '#') {
+                map_type = comments[0];
+                break;
+            }
+        }
+        // store the rest of the map based on map_type
+        if(map_type == 'M') {
+            read_map_file();
+        } else if(map_type == 'L') {
+            read_list_file();
+        } else { // TODO: get rid of this else statement when not debugging!
+            std::cout << "Invalid file format when reading type." << "\n";
+            exit(1);
+        }
+    }
+
+    void print_map() {
+        for(int i = 0; i < map_size; ++i) {
+            for(int j = 0; j < map_size; ++j) {
+                std::cout << grid[i][j].spot_type;
+            }
+            std::cout << "\n";
+        }
+    }
 };
-
-// implementation for reading in a map
-// vector<vector<point>> 
-// point: struct with land_type, been_visited
-// file will be map or list of grid coords
-
-// when the location added to the container is treasure, stop the search
-
-struct Spot {
-    // type
-    // note: unsure if there is an easier way to represent the 'type' of spot
-    // type 0: 
-    int spot_type;
-    // discovered
-    bool discovered;
-};
-
-void read_map() {
-    char map_type;
-    std::cin >> map_type;
-    std::cout << "Map type: " << map_type << "\n";
-}
-
 // 'investigation'
--- main.cpp
+++ main.cpp
@@ -10,12 +10,31 @@
 
 using namespace std;
 
+// TODO: delete debug functions 
+void DEBUG_check_options(Option_handler oh) {
+    cout << "Captain type: " << oh.get_captain_type() << "\n";
+    cout << "First-mate type: " << oh.get_firstmate_type() << "\n";
+    cout << "Hunt order: " << oh.get_hunt_order() << "\n";
+    cout << "Verbose on: " << oh.verbose_on() << "\n";
+    cout << "Stats on: " << oh.stats_on() << "\n";
+    cout << "Show path on: " << oh.show_path_on() << "\n";
+    cout << "---ALL STATS LISTED ABOVE---\n\n";
+}
+
 int main(int argc, char *argv[]) {
     ios_base::sync_with_stdio(false);
 
     Option_handler oh;
-    oh.get_options(argc, argv); 
 
-    // TODO: check that all member functions of Option_handler return the correct output when given certain arguments
-    read_map();
+    // read in all provided options
+    oh.get_options(argc, argv);
+
+    // for debug only, delete later 
+    DEBUG_check_options(oh);
+
+    // read the map into private data structure of option handler
+    oh.read_map();
+
+    // print_map() test run
+    oh.print_map();
 }
